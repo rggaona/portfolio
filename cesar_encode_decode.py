4ae838ce680dea09143f96100d6ba0d13b1c06cc
rggaona <66588159+rggaona@users.noreply.github.com>
2021-11-29 00:27:12 -0800

    Update cesar_encode_decode.py

--- cesar_encode_decode.py
+++ cesar_encode_decode.py
@@ -1,5 +1,5 @@
-import art
-print (art.logo)
+from art import logo_cesar
+print (logo_cesar)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 rerun = True
 
